3756e753bf827547360dea324b069ce173578515
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-07-19 21:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// your JS code here. If required.
+const line = document.createElement("div");
+
+line.id = "line";
+line.style.backgroundColor = "black";
+line.style.width = "200px";
+line.style.height = "2px";
+document.body.appendChild(line);
